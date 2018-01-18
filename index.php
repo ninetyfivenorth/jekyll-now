3fbf30e3feb5c4c56cafc962683c4df534ab5023
Joe Buck <31463232+ninetyfivenorth@users.noreply.github.com>
2018-01-18 03:27:54 -0500

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,6 @@
   <b>shane matthew maloney</b><br>
   <br>
   <br>
-<div style="position: relative; height: 0; overflow: hidden; padding-top:150.0%;"><iframe src="//widgets-code.websta.me/w/07f6d15edc77?ck=MjAxOC0wMS0xOFQwODoxNzowOC4wMDBa
+<div style="position: center; height: 0; overflow: hidden; padding-top:150.0%;"><iframe src="//widgets-code.websta.me/w/07f6d15edc77?ck=MjAxOC0wMS0xOFQwODoxNzowOC4wMDBa
 " class="websta-widgets" allowtransparency="true" frameborder="0" scrolling="no" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;" </iframe> <!-- webstawidgets - widgets.websta.me --></div>
 </center>
